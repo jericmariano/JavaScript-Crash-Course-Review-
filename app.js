673965c7afb190d15102be4ab041f5d5e34fffaa
Jeric <marianovjeric@gmail.com>
2023-01-09 20:13:47 -0800

    for loop on your own example

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-// DRY = don't repeat yourself 
-
-// let count = 1;
-
-// while (count <= 100) {
-//     console.log(count)
-//     count = count + 1
-// }
-
-for (let i = 1; i <= 100; i++) {
-    console.log(i)
+for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 & i % 5 === 0) {
+        console.log('Frontend Simplified')
+    }
+    else if (i % 3 === 0) {
+        console.log('Frontend')
+    }
+    else if (i % 5 === 0) {
+        console.log('Simplified')
+    }
+    else {
+        console.log(i)
+    }
 }
