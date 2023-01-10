7a197b8e253cc2d50a666adca39604386e1fc817
Jeric <marianovjeric@gmail.com>
2023-01-09 19:55:59 -0800

    first while loop

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-let cash = 50
-let price = 40
-let isStoreOpen = true
+// DRY = don't repeat yourself 
 
-let str = (cash > price) && isStoreOpen ? 'give the receipt' : 'do not give the receipt'
-console.log(str)
+let count = 1;
+
+while (count <= 100) {
+    console.log(count)
+    count = count + 1
+}
