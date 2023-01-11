850bf479a9037683f9daa6bb8b2b9e5dd595e46f
Jeric <marianovjeric@gmail.com>
2023-01-11 12:37:06 -0800

    objects review complete

--- app.js
+++ app.js
@@ -29,23 +29,23 @@ let users = [{
 }
 ]
 
-function logIn(email, password) {
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].email === email) {
-            console.log(users[i])
-            if(users[i].password === password) {
-                console.log('log the user in --- the details are correct')
-            }
-            else {
-                console.log('password is incorrect')
-            }
-            return;
-        }
-    }
-    console.log('could not find email')
-}
+// function logIn(email, password) {
+//     for (let i = 0; i < users.length; i++) {
+//         if (users[i].email === email) {
+//             console.log(users[i])
+//             if(users[i].password === password) {
+//                 console.log('log the user in --- the details are correct')
+//             }
+//             else {
+//                 console.log('password is incorrect')
+//             }
+//             return;
+//         }
+//     }
+//     console.log('could not find email')
+// }
 
-logIn('jericmariano08@gmail.com', 'test123')
+// logIn('jericmariano08@gmail.com', 'test123')
 
 // console.log(users[0].lessonsCompleted.map(elem => elem * 2))
 
@@ -54,3 +54,17 @@ logIn('jericmariano08@gmail.com', 'test123')
 // console.log(user.subscriptionStatus)
 
 // console.log(user.lessonsCompleted.map(elem => elem * 2))
+
+function register (user) {
+    users.push(user)
+}
+
+register({
+    username: 'Mark', 
+    email: 'mark@gmail.com',
+    password: 'mark123', 
+    subscriptionStatus: 'VIP', 
+    discordId: 'Mark08', 
+    lessonsCompleted: [0, 1, 2, 3, 4]})
+
+console.log(users)
