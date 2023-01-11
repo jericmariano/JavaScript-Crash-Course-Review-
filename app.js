5b26eeab2e854d791019449959d589a7d96f22b3
Jeric <marianovjeric@gmail.com>
2023-01-10 23:55:35 -0800

    map manually an array without the map method

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 let dollars = [1, 5, 10, 3]
 
-// long way
+let cents = []
 
-let cents = dollars.map( (elem) => {
-    return elem * 100
-})
-
-
-// // short way
-// let cents = dollars.map(elem => elem * 100)
+for (let i = 0; i < dollars.length; i++) {
+    cents.push(dollars[i] * 100)
+}
 
 console.log(cents)
