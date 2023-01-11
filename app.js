103cf41cf7a38058599032733a265c40d5165934
Jeric <marianovjeric@gmail.com>
2023-01-11 12:28:04 -0800

    big array of objects with functions example

--- app.js
+++ app.js
@@ -1,9 +1,56 @@
-let dollars = [1, 5, 10, 3]
+// let userFirstName = 'Jeric'
+// let userLastName = 'Mariano'
+// let userDiscordId = 'jmar08'
+// let userSubscriptionStatus = 'VIP'
 
-let cents = []
+let users = [{
+    username: 'Jeric',
+    email: 'jericmariano08@gmail.com',
+    password: 'test123',
+    subscriptionStatus: 'VIP',
+    discordId: 'jmar08',
+    lessonsCompleted: [0, 1]
+},
+{
+    username: 'Jae',
+    email: 'Jae08@gmail.com',
+    password: 'jae123',
+    subscriptionStatus: 'VIP',
+    discordId: 'Jae08',
+    lessonsCompleted: [0, 1, 2, 3]
+},
+{
+    username: 'Ze',
+    email: 'Ze08@gmail.com',
+    password: 'Ze123',
+    subscriptionStatus: 'VIP',
+    discordId: 'Ze08',
+    lessonsCompleted: [0, 1, 2, 3]
+}
+]
 
-for (let i = 0; i < dollars.length; i++) {
-    cents.push(dollars[i] * 100)
+function logIn(email, password) {
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].email === email) {
+            console.log(users[i])
+            if(users[i].password === password) {
+                console.log('log the user in --- the details are correct')
+            }
+            else {
+                console.log('password is incorrect')
+            }
+            return;
+        }
+    }
+    console.log('could not find email')
 }
 
-console.log(cents)
+logIn('jericmariano08@gmail.com', 'test123')
+
+// console.log(users[0].lessonsCompleted.map(elem => elem * 2))
+
+// console.log(user.username[0])
+
+// console.log(user.subscriptionStatus)
+
+// console.log(user.lessonsCompleted.map(elem => elem * 2))
